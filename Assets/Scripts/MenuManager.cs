d6ad1eba5bea63fc0036361cec562cf8c3d8560b
Justin Castonguay <justcastonguay@gmail.com>
2019-05-17 19:42:51 -0400

    Initial commit

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Text typedWord;
+    public InputField inputField;
+    private string typedText;
+    private RectTransform rect;
+    
+    private Vector3 rectPos;
+
+    private const string PLAY = "play", OPTIONS = "options", LEADERBOARDS = "leaderboards";
+    void Start()
+    {
+		typedWord = GameObject.FindGameObjectWithTag("Input").GetComponent<Text> ();
+        GameObject iField = GameObject.Find("InputField");
+        inputField = iField.GetComponent<InputField>();
+        rect = iField.GetComponent<RectTransform>();
+        inputField.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        typedText = inputField.text;
+        //Debug.Log(typedText);
+        GameObject iField = GameObject.Find("InputField");
+        Debug.Log(rect.localPosition.y);
+
+        GameObject enter = GameObject.Find("Enter");
+        Text enterText = enter.GetComponent<Text>();
+
+        if (typedText == PLAY){
+            rectPos = new Vector3(-203,108,0);
+            rect.localPosition = rectPos;
+            enterText.text = "Press enter";
+            if (Input.GetButtonDown("Submit")){
+                SceneManager.LoadScene("Scene1",LoadSceneMode.Single);
+            }
+            return;
+        }
+        if (typedText == OPTIONS){
+            rectPos = new Vector3(-203,48,0);
+            rect.localPosition = rectPos;
+            enterText.text = "Press enter";
+            return;
+        }
+        if (typedText == LEADERBOARDS){
+            rectPos = new Vector3(-203,-12,0);
+            rect.localPosition = rectPos;
+            enterText.text = "Press enter";
+            return;
+        }
+        enterText.text = "";
+    }
+}
---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.IO;
+
+public class TextManager : MonoBehaviour {
+
+	public string goalText;
+	public string typedText; 
+    public InputField inputField;
+    [SerializeField]
+	private Text currentWord, typedWord;
+    [SerializeField]
+    int points = 0;
+    private string wordsStr = "pension,shrink,bland,solution,feminine,policy,registration,dash,notion,lung,tourist,admire,reporter,waist,represent,poetry,define,administration,analysis,take,investment,outlet";
+    public AudioClip textComplete;
+    private AudioSource source;
+
+    private Stack<string> words;
+
+    // Use this for initialization
+    void Start () {
+        // construct stack on words, pop off each time it is correct
+
+        words = makeStack();
+
+        //goalText = GetRandomWord (wordsStr);
+        goalText = words.Peek();
+		currentWord = GameObject.FindGameObjectWithTag("Text").GetComponent<Text> ();
+		currentWord.text = goalText;
+
+		typedWord = GameObject.FindGameObjectWithTag("Input").GetComponent<Text> ();
+        GameObject iField = GameObject.Find("InputField");
+        inputField = iField.GetComponent<InputField>();
+        inputField.Select();
+
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if stack is empty -> game over
+        if (words.Count == 0){
+            Debug.Log("Stack is empty");
+            //TODO: Game Over
+            GameObject iFieldGO = GameObject.Find("InputField");
+            InputField input = iFieldGO.GetComponent<InputField>();
+            input.text = "you win";
+        }
+
+        //TODO: created equals method to ignore whitespace
+		if (typedWord != null)
+			typedText = inputField.text;
+
+        // Word spelled correctly - SFX + Word reset to new word + Text animate out of screen
+		if (goalText == typedText && words.Count > 0) {
+			Debug.Log ("Yay!");
+            source.PlayOneShot(textComplete, 1);
+            points++;
+            AnimateText();
+            UpdateScore();
+			ResetWord ();
+            }
+
+        // Resizing text 
+        // TODO: figure out way to resize text when user backspaces...  
+        if (typedWord.text.Length < inputField.text.Length)
+        {
+            typedWord.fontSize -= 10; //fix this shit to make it a ratio instead of -10 everytime   
+        }
+        
+	}
+
+	string GetRandomWord(string wordsString) {
+        
+        
+        //System.IO.File.ReadAllText(wordListPath);
+
+        string[] wordsArr = wordsStr.Split(',');
+
+        List<string> wordList = new List<string>(wordsArr);
+        foreach (string item in wordsArr)
+        {
+            //Debug.Log(item);
+        }
+
+        int randomNumberGenerator = Random.Range (0, wordsArr.Length);
+
+		return wordList [randomNumberGenerator];
+            
+	}
+
+    void UpdateScore()
+    {
+        GameObject score = GameObject.Find("Score");
+        Text scoreText = score.GetComponent<Text>();
+        scoreText.text = points.ToString();
+    }
+
+	void ResetWord() {
+
+        // Pop correct word off the stack
+        Debug.Log(words.Count);
+        words.Pop();
+		if (words.Count > 0){
+            goalText = words.Peek();
+            currentWord.text = goalText;
+
+            GameObject iFieldGO = GameObject.Find("InputField");
+            InputField input = iFieldGO.GetComponent<InputField>();
+            input.text = "" ;
+            typedWord.fontSize = 200;
+        }
+    }
+
+    void AnimateText(){
+
+    }
+
+    Stack<string> makeStack(){
+
+        string[] wordsArr = wordsStr.Split(',');
+        Stack<string> wordStack = new Stack<string>();
+        foreach (string word in wordsArr){
+            wordStack.Push(word);
+            Debug.Log(word);
+        }
+
+        return wordStack;
+    }
+}
